11c5958f6cc0a72a476e94b45ddaabfafe36cfa7
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-17 18:41:46 +0500

    Time Stamp issue resolved

--- Source.py
+++ Source.py
@@ -1,4 +1,68 @@
 
+def getdate():
+    import datetime
+    return datetime.datetime.now()
+
+def DietFile(choice):
+    if choice == 1:
+        f = open("SubhanDiet.txt", "a")
+        print("Enter the diet you want to enter in the file.")
+        diet = input()
+        str2 = "[" + str(str(getdate())) + "]" + diet
+        f.write(str2)
+        f.close()
+    if choice == 2:
+        f = open("HassaanDiet.txt", "a")
+        print("Enter the diet you want to enter in the file.")
+        diet = input()
+        str2 = "[" + str(str(getdate())) + "]" + diet
+        f.write(str2)
+        f.close()
+    if choice == 3:
+        f = open("AhmadDiet.txt", "a")
+        print("Enter the diet you want to enter in the file.")
+        diet = input()
+        str2 = "[" + str(str(getdate())) + "]" + diet
+        f.write(str2)
+        f.close()
+
+def ExcerciseFile(choice):
+    if choice == 1:
+        f = open("SubhanExcercise.txt", "a")
+        print("Enter Excercise you want to enter : ")
+        excercise = input()
+        dateandTime = str(str(getdate()))
+        str1 = "[" + dateandTime + "]" + excercise
+        f.write(str1)
+        f.close()
+    if choice == 2:
+            f = open("HassaanExcercise.txt", "a")
+            print("Enter Excercise you want to enter : ")
+            excercise = input()
+            dateandTime = str(str(getdate()))
+            str1 = "[" + dateandTime + "]" + excercise
+            f.write(str1)
+            f.close()
+    if choice == 3:
+            f = open("AhmadExcercise.txt", "a")
+            print("Enter Excercise you want to enter : ")
+            excercise = input()
+            dateandTime = str(str(getdate()))
+            str1 = "[" + dateandTime + "]" + excercise
+            f.write(str1)
+            f.close()
+
+
+def fileWrite(choice):
+    if choice == 1:
+        print("Enter 1 for write in excercise file and 2 for write in diet file.")
+        fileChoice = int(input())
+        if fileChoice == 1:
+            ExcerciseFile(choice)
+        elif fileChoice == 2:
+            DietFile(choice)
+
+
 print("Enter 0 If you want to read data from files and 1 if you want to write data to files.")
 read_Or_Write = int(input())
 if read_Or_Write == 0:
@@ -18,10 +82,11 @@ if read_Or_Write == 0:
 
         elif fileChoice == 1:
             from os.path import exists
-            file_exists = exists("SubhanExercise.txt")
+            file_exists = exists("SubhanExcercise.txt")
             if file_exists == True:
               with open("SubhanExcercise.txt") as SubhanExcercise:
-                  SubhanExcercise.read()
+                  fileRead = SubhanExcercise.read()
+                  print(fileRead)
             else:
                 print("\"Subhan Excercise\" does not exist.")
     if person_Choice == 2:
@@ -64,4 +129,9 @@ if read_Or_Write == 0:
                 print("\"Ahmad Excercise\" does not exist.")
 
 elif read_Or_Write == 1:
-    pass
+    print("Enter 1 for Subhan\nEnter 2 for Hassaan\nEnter 3 for Ahmad")
+    person_Choice = int(input())
+    fileWrite(person_Choice)
+
+
+
