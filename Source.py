88393af8b4caa40734862643557b8bad56efc327
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-06-18 00:30:10 +0500

    Code managed by defining it into functions and some minor mistakes are solved

--- Source.py
+++ Source.py
@@ -54,7 +54,6 @@ def ExcerciseFile(choice):
 
 
 def fileWrite(choice):
-    if choice == 1:
         print("Enter 1 for write in excercise file and 2 for write in diet file.")
         fileChoice = int(input())
         if fileChoice == 1:
@@ -63,70 +62,73 @@ def fileWrite(choice):
             DietFile(choice)
 
 
+def readExcerciseFile(choice):
+    if choice == 1:
+        from os.path import exists
+        file_exists = exists("SubhanExcercise.txt")
+        if file_exists == True:
+            with open("SubhanExcercise.txt") as SubhanExcercise:
+                fileRead = SubhanExcercise.read()
+                print(fileRead)
+        else:
+            print("\"Subhan Excercise\" does not exist.")
+    elif choice == 2:
+        from os.path import exists
+        file_exists = exists("HassaanExcercise.txt")
+        if file_exists == True:
+            with open("HassaanExcercise.txt") as HassaanExcercise:
+                print(HassaanExcercise.read())
+        else:
+            print("\"Hassaan Excercise\" does not exist.")
+    elif choice == 3:
+        from os.path import exists
+        file_exists = exists("AhmadExcercise.txt")
+        if file_exists == True:
+            with open("AhmadExcercise.txt") as AhmadExcercise:
+                print(AhmadExcercise.read())
+        else:
+            print("\"Ahmad Excercise\" does not exist.")
+
+def readDietFile(choice):
+    if choice == 1:
+        from os.path import exists
+        file_exists = exists("SubhanDiet.txt")
+        if file_exists == True:
+            with open("SubhanDiet.txt") as SubhanDeit:
+                print(SubhanDeit.read())
+        else:
+            print("\"Subhan Diet\" does not exist.")
+    elif choice == 2:
+        from os.path import exists
+        file_exists = exists("HassaanDiet.txt")
+        if file_exists == True:
+            with open("HassaanDiet.txt") as HassaanDeit:
+                print(HassaanDeit.read())
+        else:
+            print("\"Hassaan Diet\" does not exist.")
+    elif choice == 3:
+        from os.path import exists
+        file_exists = exists("AhmadDiet.txt")
+        if file_exists == True:
+            with open("AhmadDiet.txt") as AhmadDeit:
+                print(AhmadDeit.read())
+        else:
+            print("\"Ahmad Diet\" does not exist.")
+
+def fileRead(choice):
+    print("Enter 1 for read from Excercise file and 2 for read from diet file.")
+    fileChoice = int(input())
+    if fileChoice == 1:
+        readExcerciseFile(choice)
+    elif fileChoice == 2:
+        readDietFile(choice)
+
 print("Enter 0 If you want to read data from files and 1 if you want to write data to files.")
 read_Or_Write = int(input())
 if read_Or_Write == 0:
     print("Enter 1 for Subhan\nEnter 2 for Hassaan\nEnter 3 for Ahmad")
     person_Choice = int(input())
-    if person_Choice == 1:
-        print("Enter 0 if you want to read diet of Subhan and 1 if you want to read excercise of Subhan.")
-        fileChoice = int(input())
-        if fileChoice == 0:
-            from os.path import exists
-            file_exists = exists("SubhanDiet.txt")
-            if file_exists == True:
-                with open("SubhanDiet.txt") as SubhanDeit:
-                    SubhanDeit.read()
-            else:
-                print("\"Subhan.txt\" does not exist.")
-
-        elif fileChoice == 1:
-            from os.path import exists
-            file_exists = exists("SubhanExcercise.txt")
-            if file_exists == True:
-              with open("SubhanExcercise.txt") as SubhanExcercise:
-                  fileRead = SubhanExcercise.read()
-                  print(fileRead)
-            else:
-                print("\"Subhan Excercise\" does not exist.")
-    if person_Choice == 2:
-        print("Enter 0 if you want to read diet of Hassaan and 1 if you want to read excercise of Hassaan.")
-        fileChoice = int(input())
-        if fileChoice == 0:
-            from os.path import exists
-            file_exists = exists("HassaanDiet.txt")
-            if file_exists == True:
-                 with open("HassaanDiet.txt") as HassaanDeit:
-                    HassaanDeit.read()
-            else:
-                print("\"Hassaan Diet\" does not exist.")
-        elif fileChoice == 1:
-            from os.path import exists
-            file_exists = exists("HassaanExcercise.txt")
-            if file_exists == True:
-                with open("HassaanExcercise.txt") as HassaanExcercise:
-                    HassaanExcercise.read()
-            else:
-                print("\"Hassaan Excercise\" does not exist.")
-    if person_Choice == 3:
-        print("Enter 0 if you want to read diet of Ahmad and 1 if you want to read excercise of Ahmad.")
-        fileChoice = int(input())
-        if fileChoice == 0:
-            from os.path import exists
-            file_exists = exists("AhmadDiet.txt")
-            if file_exists == True:
-                with open("AhmadDiet.txt") as AhmadDeit:
-                    AhmadDeit.read()
-            else:
-                print("\"Ahmad Diet\" does not exist.")
-        elif fileChoice == 1:
-            from os.path import exists
-            file_exists = exists("AhmadExcercise.txt")
-            if file_exists == True:
-                with open("AhmadDiet.txt") as AhmadExcercise:
-                    AhmadExcercise.read()
-            else:
-                print("\"Ahmad Excercise\" does not exist.")
+    fileRead(person_Choice)
 
 elif read_Or_Write == 1:
     print("Enter 1 for Subhan\nEnter 2 for Hassaan\nEnter 3 for Ahmad")
